55ae1349527e04b9247a0d9d7a58931d9b4c1886
Syriamanal <Shadinachat123@gmail.com>
2013-12-25 15:33:08 -0800

    Added more stuffy :D

--- C++.cpp
+++ C++.cpp
@@ -12,13 +12,13 @@ int age;
 cout << "Enter Your age..." << endl;
     cout << endl;
 cin >> age;
-cout << "You have entered  " << age << endl;
+cout << "You have entered " << age << "!" <<endl;
 
 //This will be the output...
 if(age == 18)
 {
     cout << endl;
-    cout << "You are a teenager" << endl;
+    cout << "You are a adult" << endl;
 }
 else if(age >= 18)
 {
@@ -30,16 +30,40 @@ else if(age == 17)
     cout << endl;
     cout << "One more year to be 18!" << endl;
 }
+else if(age == 16)
+{
+    cout << endl;
+    cout << "Two more years to be 18!" << endl;
+}
+else if(age == 15)
+{
+    cout << endl;
+    cout << "Three more years to be 18!" << endl;
+}
 else if(age == 14)
 {
     cout << endl;
-    cout << "You are in high school!" << endl;
+    cout << "Four more years to be 18!" << endl;
+}
+else if(age == 13)
+{
+    cout << endl;
+    cout << "You are " << age << endl;
+}else if(age == 12)
+{
+    cout << endl;
+    cout << "Don't you have homework to do?" << endl;
+}
+else if(age == 11)
+{
+    cout << endl;
+    cout << "Did you do your homework?" << endl;
 }
 else
 {
     //This will procec when age is bellow 14...
     cout << endl;
-    cout << "Your to young!\a";
+    cout << "Your to young!\a" << endl;
     cout << "You need to do your homework!\a" << endl;
 }
 }
